fc2e767f70a197bd4b2148d5e19297db4110acd3
Roger Rajaratnam <roger@sourcier.uk>
2021-02-26 10:21:58 +0000

    ✨ Display Note

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 
 export default function Routes() {
   return (
@@ -14,6 +15,9 @@ export default function Routes() {
       </Route>
       <Route exact path="/notes/new">
         <NewNote />
+      </Route>
+      <Route exact path="/notes/:id">
+        <Notes />
       </Route>
       <Route exact path="/login">
         <Login />
