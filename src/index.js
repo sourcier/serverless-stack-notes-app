7f25436a2de49d1a9fc8266bba76319e67d8edc1
Roger Rajaratnam <roger@sourcier.uk>
2021-02-26 14:36:52 +0000

    🔊 Setup Error Reporting in React

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import config from './config';
+import { initSentry } from './libs/errorLib';
+
+initSentry();
 
 Amplify.configure({
   Auth: {
--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,3 +1,26 @@
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.init({ dsn: "https://ec0b72032aea49b6b7eae6a1eb016fe7@o375554.ingest.sentry.io/5653648" });
+}
+
+export function logError(error, errorInfo = null) {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.withScope((scope) => {
+    errorInfo && scope.setExtras(errorInfo);
+    Sentry.captureException(error);
+  });
+}
+
 export function onError(error) {
   let message = error.toString();
 
