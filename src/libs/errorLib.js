b892ae41cb7879e4a692e65c1f2f35bfebd4ede1
Roger Rajaratnam <roger@sourcier.uk>
2021-02-26 14:39:06 +0000

    🔊 Report API Errors in React

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -22,12 +22,20 @@ export function logError(error, errorInfo = null) {
 }
 
 export function onError(error) {
+  let errorInfo = {};
   let message = error.toString();
 
   // Auth errors
   if (!(error instanceof Error) && error.message) {
+    errorInfo = error;
     message = error.message;
+    error = new Error(message);
+    // API errors
+  } else if (error.config && error.config.url) {
+    errorInfo.url = error.config.url;
   }
+
+  logError(error, errorInfo);
 
   alert(message);
 }
