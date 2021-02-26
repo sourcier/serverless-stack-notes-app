14721b5032a3701bce53fd6634ea3cd8dde6ee95
Roger Rajaratnam <roger@sourcier.uk>
2021-02-26 10:26:22 +0000

    ✨ Delete Note

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -90,6 +90,10 @@ export default function Notes() {
     }
   }
 
+  function deleteNote() {
+    return API.del("notes", `/notes/${id}`);
+  }
+
   async function handleDelete(event) {
     event.preventDefault();
 
@@ -102,6 +106,14 @@ export default function Notes() {
     }
 
     setIsDeleting(true);
+
+    try {
+      await deleteNote();
+      history.push("/");
+    } catch (e) {
+      onError(e);
+      setIsDeleting(false);
+    }
   }
 
   return (
