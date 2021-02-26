78ce4f02fc311495fbfa00551d81d526604e5ec5
Roger Rajaratnam <roger@sourcier.uk>
2021-02-26 10:15:26 +0000

    ✨ List All the Notes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import { useAppContext } from "../libs/contextLib";
+import { onError } from "../libs/errorLib";
 import "./Home.css";
 
 export default function Home() {
-  return (
-    <div className="Home">
+  const [notes, setNotes] = useState([]);
+  const { isAuthenticated } = useAppContext();
+  const [isLoading, setIsLoading] = useState(true);
+
+  function renderNotesList(notes) {
+    return null;
+  }
+
+  function renderLander() {
+    return (
       <div className="lander">
         <h1>Scratch</h1>
         <p className="text-muted">A simple note taking app</p>
       </div>
+    );
+  }
+
+  function renderNotes() {
+    return (
+      <div className="notes">
+        <h2 className="pb-3 mt-4 mb-3 border-bottom">Your Notes</h2>
+        <ListGroup>{!isLoading && renderNotesList(notes)}</ListGroup>
+      </div>
+    );
+  }
+
+  return (
+    <div className="Home">
+      {isAuthenticated ? renderNotes() : renderLander()}
     </div>
   );
 }
