dc07e46c3d9d27eeef2e6d33e546cb65e0058975
Roger Rajaratnam <roger@sourcier.uk>
2021-02-26 15:57:42 +0000

    🔧 Manage Environments

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-const config = {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   STRIPE_KEY: "pk_test_cA7dgir7RDXeSfI1ehdPDRxy",
   s3: {
     REGION: "eu-west-2",
@@ -15,6 +14,31 @@ const config = {
     APP_CLIENT_ID: "3k563fsh4678drplb2if7pfhje",
     IDENTITY_POOL_ID: "eu-west-2:fefa348e-3328-4d9d-ac57-2787230c6b05",
   },
+};
+
+const prod = {
+  STRIPE_KEY: "YOUR_STRIPE_PROD_PUBLIC_KEY",
+  s3: {
+    REGION: "YOUR_PROD_S3_UPLOADS_BUCKET_REGION",
+    BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
+  },
+  apiGateway: {
+    REGION: "YOUR_PROD_API_GATEWAY_REGION",
+    URL: "YOUR_PROD_API_GATEWAY_URL"
+  },
+  cognito: {
+    REGION: "YOUR_PROD_COGNITO_REGION",
+    USER_POOL_ID: "YOUR_PROD_COGNITO_USER_POOL_ID",
+    APP_CLIENT_ID: "YOUR_PROD_COGNITO_APP_CLIENT_ID",
+    IDENTITY_POOL_ID: "YOUR_PROD_IDENTITY_POOL_ID"
+  }
+};
+
+const config = {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  // Default to dev if not set
+  ...(process.env.REACT_APP_STAGE === "prod" ? prod : dev),
 };
 
 export default config;
