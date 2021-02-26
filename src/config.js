3fd597f97694d74981324d1b1db2217ab3641c13
Roger Rajaratnam <roger@sourcier.uk>
2021-02-26 16:30:40 +0000

    🔧 Update dev config

--- src/config.js
+++ src/config.js
@@ -2,32 +2,32 @@ const dev = {
   STRIPE_KEY: "pk_test_cA7dgir7RDXeSfI1ehdPDRxy",
   s3: {
     REGION: "eu-west-2",
-    BUCKET: "53mecql-serverless-stack-notes",
+    BUCKET: "dev-serverless-stack-notes-infra-uploads4f6eb0fd-nf7e7dh1i14z",
   },
   apiGateway: {
     REGION: "eu-west-2",
-    URL: "https://v15evm9485.execute-api.eu-west-2.amazonaws.com/prod",
+    URL: "https://28vwuonn5g.execute-api.eu-west-2.amazonaws.com/dev",
   },
   cognito: {
     REGION: "eu-west-2",
-    USER_POOL_ID: "eu-west-2_FP1HnZwMu",
-    APP_CLIENT_ID: "3k563fsh4678drplb2if7pfhje",
-    IDENTITY_POOL_ID: "eu-west-2:fefa348e-3328-4d9d-ac57-2787230c6b05",
+    USER_POOL_ID: "eu-west-2_FFqhufieN",
+    APP_CLIENT_ID: "1ol1378ac4lfu7h06dqbaq0v2l",
+    IDENTITY_POOL_ID: "eu-west-2:c23af4dc-abf3-43b9-92b2-7cd459c08618",
   },
 };
 
 const prod = {
-  STRIPE_KEY: "YOUR_STRIPE_PROD_PUBLIC_KEY",
+  STRIPE_KEY: "pk_test_cA7dgir7RDXeSfI1ehdPDRxy",
   s3: {
-    REGION: "YOUR_PROD_S3_UPLOADS_BUCKET_REGION",
+    REGION: "eu-west-2",
     BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
   },
   apiGateway: {
-    REGION: "YOUR_PROD_API_GATEWAY_REGION",
+    REGION: "eu-west-2",
     URL: "YOUR_PROD_API_GATEWAY_URL"
   },
   cognito: {
-    REGION: "YOUR_PROD_COGNITO_REGION",
+    REGION: "eu-west-2",
     USER_POOL_ID: "YOUR_PROD_COGNITO_USER_POOL_ID",
     APP_CLIENT_ID: "YOUR_PROD_COGNITO_APP_CLIENT_ID",
     IDENTITY_POOL_ID: "YOUR_PROD_IDENTITY_POOL_ID"
